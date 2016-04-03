eae1cc06d3d3873a50545b70a9755f67b693374c
Prabhat Sharma <hi.prabhat@gmail.com>
2016-04-03 23:06:20 +0530

    Fixed several bugs. Now it checks for wild cards from 1-4 characters. Added comments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var CryptoJS = require("crypto-js");
 var fs = require("fs");
 var argv = require('minimist')(process.argv.slice(2));
 var eyes = require("eyes");
+var moment = require("moment")
 
 var utils = require("./src/utils");
 
@@ -11,82 +12,216 @@ var walletFile = argv.walletFile;
 var passwordFile = argv.passwordFile;
 var knownString = argv.knownString;
 var passwordPosition = argv.passwordPosition;
+var showTrials;
+
+if(argv.showTrials=="true") true
+else showTrials=false
 
 var passwordFound = false;
 
-var currentCount = 0;   //Current stats of password trial
+var currentCount = 0; //Current stats of password trial
 var currentPassword;
 
 var globalWallet;
 
-console.log("\n Usage is : node app.js --walletFile=ethwallet.json [--passwordFile=passwordlist.txt] --knownString=<kString> --passwordPostion=<number>");
+console.log("\n Usage is : node app.js --walletFile=ethwallet.json [--passwordFile=passwordlist.txt] --knownString=<kString> --passwordPostion=<number> --showTrials=true/false");
 
 fs.readFile(walletFile, 'utf-8', function name(error, wallet) {
-    globalWallet=wallet;
+    globalWallet = wallet;
     //console.log(JSON.parse(wallet));
-    if(passwordFile){
+    if (passwordFile) {
         fs.readFile(passwordFile, 'utf-8', function name(error, passwords) {
             passwords = passwords.split("\n");
-            
+
             console.time("decryptWallet");
-            
-            for(var i=0;i<passwords.length;i++) {
-                if(utils.decryptPresaleKey(JSON.parse(wallet), passwords[i])){
+
+            for (var i = 0; i < passwords.length; i++) {
+                console.log("Trying password : " + passwords[i]);
+                if (utils.decryptPresaleKey(JSON.parse(wallet), passwords[i])) {
                     console.log("Password found. It is: " + passwords[i]);
                     passwordFound = true;
                     break;
                 }
             }
-            
-            if(!passwordFound)
+
+            if (!passwordFound)
                 console.log("Dictionary attack failed");
-            
+
             console.timeEnd("decryptWallet");
             console.log("attacked the wallet with dictionary of : " + passwords.length + " passwords");
         });
     } else {
-        bruteForce(knownString,passwordPosition);
+        console.time("bruteForce")
+        
+        if(!bruteForce1(knownString, passwordPosition, showTrials)) //Try brute force with 1 wildcard character
+            if(!bruteForce2(knownString, passwordPosition, showTrials)) //Try brute force with 2 wildcard characters if using 1 wildcard fails
+                if(!bruteForce3(knownString, passwordPosition, showTrials)) //Try brute force with 3 wildcard characters if using 2 wildcard fails
+                    bruteForce4(knownString, passwordPosition, showTrials)  //Try brute force with 4 wildcard characters if using 3 wildcard fails
+        
+        console.timeEnd("bruteForce")
+        
     }
-    
-
-}) 
 
 
+})
 
-function bruteForce(kString, position) {
-    
+function bruteForce1(kString, position, showTrials) {
+    console.time("bruteForce1")
+
     knownString = kString;
     passwordPosition = position;
-    
-    console.log("Trying " + 94*94*94 + " passwords")
-    
+
+    console.log("Trying 1 wildcard (94) passwords") // http://www.asciitable.com/
+
     var password = "";
     var part1OfPassword = "";
     var part2OfPassword = "";
+
+    //Doing it for all printable characters
+
+    //1 possible wildcards will be tested
+
+    for (var d = 32; d < 126; d++) {
+        password = String.fromCharCode(d);
+        part1OfPassword = password.substr(0, position - 1);
+        part2OfPassword = password.substr(position - 1, password.length);
+        currentPassword = part1OfPassword + kString + part2OfPassword;
+
+        if(showTrials) console.log("Trying password : " + currentPassword);
+
+        currentCount = (d - 31);
+
+        if (utils.decryptPresaleKey(JSON.parse(globalWallet), currentPassword)) {
+            console.log("Password found. It is: " + currentPassword);
+            console.timeEnd("bruteForce1")
+            return true;
+        }
+    }
+    console.timeEnd("bruteForce1")
     
-    //Doing it only till 125. ~ is giving trouble. Will deal with it later
-    for(var a=32;a<=126;a++){
-        for(var b=32;b<=126;b++){
-            for(var c=32;c<=126;c++){
-                for(var d=0;d<126;d++){
-                    password = String.fromCharCode(a) + String.fromCharCode(b) + String.fromCharCode(c) + String.fromCharCode(c);
-                    part1OfPassword = password.substr(0,position-1);
-                    part2OfPassword = password.substr(position-1, password.length);
-                    currentPassword = part1OfPassword + kString + part2OfPassword;
-                    
-                    currentCount = (a-31)*(b-31)*(c-31)*(d-31);
-                    
-                    if(utils.decryptPresaleKey(JSON.parse(globalWallet), currentPassword)){
-                        console.log("Password found. It is: " + passwords[i]);
-                        passwordFound = true;
-                        break;
-                    }
-                    //console.log(currentPassword);
+    return false;
+}
+
+function bruteForce2(kString, position, showTrials) {
+    console.time("bruteForce2")
+
+    knownString = kString;
+    passwordPosition = position;
+
+    console.log("Trying 2 wildcards (94*94 = 8836) passwords")
+
+    var password = "";
+    var part1OfPassword = "";
+    var part2OfPassword = "";
+
+    //Doing it for all printable characters
+
+    //2 possible wildcards will be tested
+
+    for (var c = 32; c <= 126; c++) {
+        for (var d = 32; d < 126; d++) {
+            password = String.fromCharCode(c) + String.fromCharCode(d);
+            part1OfPassword = password.substr(0, position - 1);
+            part2OfPassword = password.substr(position - 1, password.length);
+            currentPassword = part1OfPassword + kString + part2OfPassword;
+
+            if(showTrials) console.log("Trying password : " + currentPassword);
+
+            currentCount = (c - 31) * (d - 31);
+
+            if (utils.decryptPresaleKey(JSON.parse(globalWallet), currentPassword)) {
+                console.log("Password found. It is: " + currentPassword);
+                console.timeEnd("bruteForce2")
+                return true;
+            }
+        }
+    }
+    console.timeEnd("bruteForce2")
+
+    return false;
+}
+
+function bruteForce3(kString, position, showTrials) {
+    
+    console.time("bruteForce3")
+
+    knownString = kString;
+    passwordPosition = position;
+
+    console.log("Trying 3 wildcards (94*94*94 = 830,584) passwords")
+
+    var password = "";
+    var part1OfPassword = "";
+    var part2OfPassword = "";
+
+    //Doing it for all printable characters
+
+    //3 possible wildcards will be tested
+    for (var b = 32; b <= 126; b++) {
+        for (var c = 32; c <= 126; c++) {
+            for (var d = 32; d < 126; d++) {
+                password = String.fromCharCode(b) + String.fromCharCode(c) + String.fromCharCode(d);
+                part1OfPassword = password.substr(0, position - 1);
+                part2OfPassword = password.substr(position - 1, password.length);
+                currentPassword = part1OfPassword + kString + part2OfPassword;
+
+                if(showTrials) console.log("Trying password : " + currentPassword);
+
+                currentCount = (b - 31) * (c - 31) * (d - 31);
+
+                if (utils.decryptPresaleKey(JSON.parse(globalWallet), currentPassword)) {
+                    console.log("Password found. It is: " + currentPassword);
+                    console.timeEnd("bruteForce3")
+                    return true;
                 }
             }
         }
     }
     
-    return true;
+    console.timeEnd("bruteForce3")
+
+    return false;
 }
 
+
+function bruteForce4(kString, position, showTrials) {
+    console.time("bruteForce4")
+
+    knownString = kString;
+    passwordPosition = position;
+
+    console.log("Trying 4 wildcards (94*94*94*94 = 78,074,896) passwords")
+
+    var password = "";
+    var part1OfPassword = "";
+    var part2OfPassword = "";
+
+    //Doing it for all printable characters
+
+    //4 possible wildcards will be tested
+    for (var a = 32; a <= 126; a++) {
+        for (var b = 32; b <= 126; b++) {
+            for (var c = 32; c <= 126; c++) {
+                for (var d = 32; d < 126; d++) {
+                    password = String.fromCharCode(a) + String.fromCharCode(b) + String.fromCharCode(c) + String.fromCharCode(d);
+                    part1OfPassword = password.substr(0, position - 1);
+                    part2OfPassword = password.substr(position - 1, password.length);
+                    currentPassword = part1OfPassword + kString + part2OfPassword;
+
+                    if(showTrials) console.log("Trying password : " + currentPassword);
+
+                    currentCount = (a - 31) * (b - 31) * (c - 31) * (d - 31);
+
+                    if (utils.decryptPresaleKey(JSON.parse(globalWallet), currentPassword)) {
+                        console.log("Password found. It is: " + currentPassword);
+                        console.timeEnd("bruteForce4")
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    console.timeEnd("bruteForce4")
+
+    return false;
+}
--- src/utils.js
+++ src/utils.js
@@ -3,59 +3,41 @@
 var CryptoJS = require("crypto-js");
 var pbkdf2 = require("pbkdf2");
 var ethUtil = require("ethereumjs-util");
-
+/**
+ * This functions tries a password to an ethereum wallet and return trueness/falseness of password
+ * @param  {object} presaleJson - Presale JSON wallet object
+ * @param  {string} presalepass - presale password to be tried
+ * @returns true=if password is able to decrypt the wallet, false otherwise
+ */
 exports.decryptPresaleKey = function(presaleJson, presalepass) {
-        //presaleJson = JSON.parse(presaleJson);
-        var encSeedBytes = this.hexToBytes(presaleJson.encseed);
-        var iv = this.hexToCryptoJS(this.bytesToHex(encSeedBytes.slice(0, 16)));
-        var cipherText = this.hexToCryptoJS(this.bytesToHex(encSeedBytes.slice(16)));
-        
-        //var derivedKey = this.sha256.pbkdf2(this.stringToBytes(presalepass), this.stringToBytes(presalepass), 2000, 16);
-        var derivedKey = pbkdf2.pbkdf2Sync(presalepass, presalepass, 2000, 16, 'sha256')
-        //var derivedKey = CryptoJS.PBKDF2(this.stringToBytes(presalepass), this.stringToBytes(presalepass), 2000, 16)
-        
-        var passbytes = this.hexToBytes(this.bytesToHex(derivedKey)).slice(0, 16);
-        var plainText = CryptoJS.AES.decrypt({
-            ciphertext: cipherText,
-        }, this.hexToCryptoJS(this.bytesToHex(passbytes)), {
-            iv: iv,
-            mode: CryptoJS.mode.CBC,
-            padding: CryptoJS.pad.Pkcs7,
-        });
-        var plainTextHex = this.cryptoJSToHex(plainText);
-        plainText = this.hex2str(plainTextHex);
-        var ethPriv = CryptoJS.SHA3(plainText, {
-            outputLength: 256
-        });
-        var privkey = ethPriv.toString();
-        if(this.verifyPrivKey(privkey, presaleJson.ethaddr)) {
-            //console.log("Password found : " + presalepass);
-            return true;
-        }
-        else
-            return false;
-    }
-    
-exports.hex2str = function(hex) {
+
+    var hex2str = function(hex) {
         var hex = hex.toString();
         var str = '';
         for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
         return str;
     }
-
-exports.hexToBytes = function (hex) {
+    /**
+     * Convert a hexadecimal encoded string into an array of integers
+     * @param  {string} hex hexadecimal encoded string
+     * @returns {array} bytes - array of integers
+     */
+    var hexToBytes = function(hex) {
         for (var bytes = [], c = 0; c < hex.length; c += 2)
-        bytes.push(parseInt(hex.substr(c, 2), 16));
+            bytes.push(parseInt(hex.substr(c, 2), 16));
         return bytes;
     }
 
-    
-    exports.hexToCryptoJS = function (x) {
+
+    var hexToCryptoJS = function(x) {
         return CryptoJS.enc.Hex.parse(x);
     }
-
-    exports.bytesToHex = function(bytes) {
+    /**
+     * Convert bytes into hex values
+     * @param  {any} bytes
+     */
+    var bytesToHex = function(bytes) {
         for (var hex = [], i = 0; i < bytes.length; i++) {
             hex.push((bytes[i] >>> 4).toString(16));
             hex.push((bytes[i] & 0xF).toString(16));
@@ -63,33 +45,69 @@ exports.hexToBytes = function (hex) {
         return hex.join("");
     }
 
-    exports.stringToBytes = function(str) {
-        var bytes = [];
-        for (var i = 0; i < str.length; ++i) {
-            bytes.push(str.charCodeAt(i));
-        }
-        return bytes;
+    var cryptoJSToHex = function(x) {
+        return CryptoJS.enc.Hex.stringify(x);
     }
 
-exports.sha256 = function (a) {
-  a = exports.toBuffer(a)
-  return CryptoJS.createHash('SHA256').update(a).digest()
-}
+    var verifyPrivKey = function(privkey, address) {
+        if (privkey.length != 64)
+            return false;
+        if (strPrivateKeyToAddress(privkey) != address)
+            return false;
+        else
+            return true;
+    }
 
-exports.cryptoJSToHex = function(x) {
-	return CryptoJS.enc.Hex.stringify(x);
-}
+    var strPrivateKeyToAddress = function(privkey) {
+        var publicKey = ethUtil.privateToPublic(new Buffer(privkey, 'hex'));
+        return ethUtil.publicToAddress(publicKey).toString('hex');
+    }
 
-exports.verifyPrivKey = function (privkey, address){
-    if(privkey.length!=64)
-        return false;
-    if(this.strPrivateKeyToAddress(privkey)!=address)
-        return false;
-    else
+    /***************************************************************************
+     * Actual program execution begins from here
+     ****************************************************************************/
+    // var encSeedBytes = hexToBytes(presaleJson.encseed);     //Get the hexadeciml encoded encrypted seed and convert it into an array of 96 bytes
+    // var iv = hexToCryptoJS(bytesToHex(encSeedBytes.slice(0, 16)));
+    // var cipherText = hexToCryptoJS(bytesToHex(encSeedBytes.slice(16)));
+    
+    // encseed = 16 bytes initialization vector + 80 bytes ciphertext
+    
+    //Get the initialization vector from the encrypted seed. First 16 bytes (32 hex encoded chars)
+    var iv = hexToCryptoJS(presaleJson.encseed.substr(0,32));   
+    
+    //Get the ciphertext from encrypted seed. Next 80 bytes (160 hex encoded chars)
+    var cipherText = hexToCryptoJS(presaleJson.encseed.substr(32,160)); 
+    
+    //Create the derived key from presalepassword
+    var derivedKey = pbkdf2.pbkdf2Sync(presalepass, presalepass, 2000, 16, 'sha256')
+    var passbytes = hexToBytes(bytesToHex(derivedKey)).slice(0, 16);
+    
+    //Get the private key from derived key
+    //AES.decrypt(ciphertext, key, cfg)
+    var plainText = CryptoJS.AES.decrypt({
+        ciphertext: cipherText,
+    }, hexToCryptoJS(bytesToHex(passbytes)), {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7,
+    });
+    var plainTextHex = cryptoJSToHex(plainText);
+    plainText = hex2str(plainTextHex);
+    
+    var ethPriv = CryptoJS.SHA3(plainText, {
+        outputLength: 256
+    });
+    var privkey = ethPriv.toString();
+    //We got the private key at this point
+    
+    
+    
+    //Generate public address(ethaddr) that was generated using private key and compare against 
+    //public address to verify the correctness of private key
+    if (verifyPrivKey(privkey, presaleJson.ethaddr)) {
+        //console.log("Password found : " + presalepass);
         return true;
-}
-
-exports.strPrivateKeyToAddress = function(privkey){
-    var publicKey = ethUtil.privateToPublic(new Buffer(privkey, 'hex'));
-    return ethUtil.publicToAddress(publicKey).toString('hex');
+    } else {
+        return false;
+    }
 }
